1becf52761b7d87c1927c9b6194a310c4d7da024
blazecam1 <>
2021-05-24 20:45:49 +0000

    Updated Candidate testing to Candidate Testing part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = ("");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = ("Who was the first American woman in space? ");
+let correctAnswer;("Sally Ride");
+let candidateAnswer = ("")
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,12 +15,17 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+let info = input.question("Candidate Name: ")
+
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+ let askQuestion = input.question("Who was the first American woman in space? ");
+  if (input.question === correctAnswer) {
+    console.log("Sally Ride")
+  }
 
 }
 
@@ -36,9 +41,10 @@ function gradeQuiz(candidateAnswers) {
 }
 
 function runProgram() {
-  askForName();
+  askForName(); info = String(info);
+
   // TODO 1.1c: Ask for candidate's name //
-  
+  askForName(); console.log( candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
